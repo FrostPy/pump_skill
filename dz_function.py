3e5ac88f9e2a2eb72b1cc1c3ae6d3b16d4682b46
Frost <ua_frost@mail.ru>
2022-04-10 11:57:38 +0300

    new commit

--- dz_function.py
+++ dz_function.py
@@ -1,6 +1,6 @@
 def fizz_buzz(m,n):
     s = 0
-    for i in range(m,n + 1):
+    for i in range(m,n):
         if i % 3 == 0 and i % 5 == 0:
             s += i
     return print(s) 
@@ -11,7 +11,9 @@ def fizz_buzz(m,n):
 def plural_form(number:int,form_1:str,form_2:str,form_3:str):
     c = list(str(number))
     #print(c)
-    if 5 <= number <= 20:
+    if number == 1:
+        return form_1
+    elif 5 <= number <= 20:
         return form_3
     elif number > 20:
         if 2 <=int(c[-1]) <= 4:
@@ -21,7 +23,7 @@ def plural_form(number:int,form_1:str,form_2:str,form_3:str):
         elif int(c[-1]) == 0:
             return form_3
         if 5 <=int(c[-1]) <= 9:
-             return form_3    
+            return form_3 
 
 
 
