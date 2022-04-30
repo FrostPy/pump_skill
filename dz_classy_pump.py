75395b6572c82c3d79aabd6ab7285258c484786d
Frost <ua_frost@mail.ru>
2022-04-30 17:11:04 +0300

    bew

--- dz_classy_pump.py
+++ dz_classy_pump.py
@@ -1,5 +1,3 @@
-
-
 class Product:
     def __init__(self, title, calorific, cost):
         self.__title = title
@@ -10,7 +8,6 @@ class Product:
     def title(self):
         return self.__title
 
-    
     @title.setter
     def title(self, value):
         if not value:
@@ -25,7 +22,7 @@ class Product:
     @calorific.setter
     def calorific(self, value):
         if value <= 0:
-            raise ValueError('Значение атрибута calorific только положительное число')
+            raise ValueError('Калорийность должна быть положительной')
         else:
             self.__calorific = value
 
@@ -36,33 +33,71 @@ class Product:
     @cost.setter
     def cost(self, value):
         if value <= 0:
-            raise ValueError('Значение cost только положительное число')
+            raise ValueError('Себестоимость должна быть положительной')
         else:
-            self.__cost = value                
+            self.__cost = value
 
-class Ingredient:
+
+class Ingredient():
     def __init__(self, product, weight):
             self.product = product
             self.__weight = weight
 
-    @property        
+    @property
     def weight(self):
         return self.__weight
-    
+
     @weight.setter
-    def weight(self,value):
-        if self.__weight <= 0:
-            raise ValueError('Значение атрибута weight должно быть положительным')   
+    def weight(self, value):
+        if value <= 0:
+            raise ValueError('Вес должен быть положительным')
         else:
             self.__weight = value
-    
-    def get_calorific(self):
-        calorific_ingredient = self.weight / 100 * self.product.calorific
-        return calorific_ingredient
-    
-    
-    def get_cost(self):
-        cost_ingredient = self.weight / 100 * self.product.cost
-        return cost_ingredient
 
+    def get_calorific(self):
+        return self.weight / 100 * self.product.calorific
+
+    def get_cost(self):
+        return self.weight / 100 * self.product.cost
+
+
+class Pizza (Product):
+    def __init__(self, title, ingredients):
+        super().__init__(title, 1, 1)
+        self.ingredients = ingredients
+
+    def get_calorific(self):
+        total_calorific = 0
+        for ingredient in self.ingredients:
+            total_calorific += ingredient.get_calorific()
+        return total_calorific
+
+    def get_cost(self):
+        total_cost = 0
+        for ingredient in self.ingredients:
+            total_cost += ingredient.get_cost()
+        return total_cost
+
+    def __str__(self):
+        return f'{self.title} ({self.get_calorific()} kkal) - {self.get_cost()} руб'
+
+
+# Создаем продукты с указанием названия, калорийности продукта и его себестоимости
+dough_product = Product('Тесто', 200, 20)
+tomato_product = Product('Помидор', 100, 50)
+cheese_product = Product('Сыр', 100, 120)
+
+# Из продуктов создаем ингредиенты.
+# Для каждого ингредиента указываем продукт, из которого он состоит и вес продукта
+dough_ingredient = Ingredient(dough_product, 100)
+tomato_ingredient = Ingredient(tomato_product, 100)
+cheese_ingredient = Ingredient(cheese_product, 100)
+
+# Из ингредиентов создаем пиццу
+pizza_margarita = Pizza('Маргарита', [dough_ingredient, tomato_ingredient, cheese_ingredient])
+pizza_margarita_light = Pizza('Маргарита', [dough_ingredient, cheese_ingredient])
+
+# Выводим экземпляр пиццы
+print(pizza_margarita)
+print(pizza_margarita_light)
 
